cb73f1bcebbf5a0dd90232b0cc05a90a3dcb8dd3
thang21197 <48194900+thang21197@users.noreply.github.com>
2020-03-21 00:48:05 +0700

    update

--- src/Component/ListItemsComponent.js
+++ src/Component/ListItemsComponent.js
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class ListItemsComponent extends Component {
+    componentWillMount(){
+        this.setState({
+            isCompleted:this.props.isCompleted
+        });
+    }
     isClick= (event) =>{
       event.preventDefault();
       this.props.removeItem(this.props.idItem);
     }
+    isChecked  = (event) =>{
+        // this.setState({
+        //     isCompleted:!this.props.isCompleted
+        // });
+    }
+    isShowItem=()=>{
+        if(this.state.isCompleted===false){
+            return(<li><input onChange={(event) =>{this.isChecked(event)}} className="checkbox" type="checkbox" /> <span className="todo-text">{this.props.content}</span><a href="/" onClick={(e)=>{this.isClick(e)}} className="remove text-right"><i className="fa fa-trash" /></a><hr /></li>)
+        }
+        if(this.state.isCompleted===true){
+            return(<li><input onChange={(event) =>{this.isChecked(event)}} className="checkbox" type="checkbox" checked/> <span className="todo-text checked">{this.props.content}</span><a href="/" onClick={(e)=>{this.isClick(e)}} className="remove text-right"><i className="fa fa-trash" /></a><hr /></li>)
+        }
+      
+    }
     render() {
+        
         return (
             <div>
-                 <li><input className="checkbox" type="checkbox" /> <span className="todo-text">{this.props.content}</span><a href="/" onClick={(e)=>{this.isClick(e)}} className="remove text-right"><i className="fa fa-trash" /></a><hr /></li>
+                 {this.isShowItem()}
             </div>
         );
     }
